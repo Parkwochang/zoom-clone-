90f0bf164ebff179571cd129e79a2e0a45951b99
Parkwochang <whwh3560@gmail.com>
2022-05-02 18:57:27 +0900

    video chat

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from "http";
-import {Server} from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import WebSocket from "ws";
 import express from "express";
+import { instrument } from "@socket.io/admin-ui";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -14,63 +15,21 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const io = new Server(httpServer,  {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true
-  }
-});
-
-instrument(io, {
-  auth: false
-});
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: {sids,rooms},
-    },
-  } = io;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if(sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return io.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-io.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  io.sockets.emit("room_change", publicRooms());
-
-  socket.onAny((event) => {
-    console.log(`socket Event: ${event}`);
-  })
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done(countRoom(roomName));
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    io.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => 
-      socket.to(room).emit("bye", socket.nickname, countRoom(roomName)-1)
-      );
-  });
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-  socket.on("change_nickname", (change) => socket["nickname"] = change);
-});
-
+const io =  new Server(httpServer);
 httpServer.listen(3000, handleListen);
+
+io.on("connection", socket => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
